c19f095852579a4e21483eac027808553672875e
Justin Hortopanu <youdoge21@gmail.com>
2024-09-12 15:30:39 -0500

    Adding possibly required files for program

---
+++ constellation.h
@@ -0,0 +1,23 @@
+/*\
+ *  Author: ...
+ *  Class: ...
+ *  Description: ...
+ */
+
+#ifndef CONSTELLATION_H
+#define CONSTELLATION_CPP
+
+#include <iostream>
+#include <string>
+
+class constellation{
+    private:
+        string name;
+        string comboStars[];
+        string description;
+
+    public:
+        //to be determined
+};
+
+#endif
---
+++ driver.cpp
@@ -0,0 +1,18 @@
+/*\
+ *  Author: ...
+ *  Class: ...
+ *  Description: ...
+ */
+
+#include <iostream>
+#include <string>
+#include "constellation.h"
+
+const int NUMCONSTELLATIONS = 48;
+
+int main(){
+    constellation arr[NUMCONSTELLATIONS];
+    std::cout << "Hello there.";
+
+    return 0;
+}
